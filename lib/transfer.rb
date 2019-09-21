a0c921a28f398e05309b67fe8a51b07e3dc2baa8
John Guest <jcguest90@gmail.com>
2019-09-21 18:47:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.stus == "complete"
+    if self.status == "complete"
     sender.balance += amount
     receiver.balance -= amount
     self.status = "reversed"
